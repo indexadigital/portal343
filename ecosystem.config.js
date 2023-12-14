b77c3017fd852444fd59e488fb6f3aff42dcdd15
indexadigital <indexadigital.com@gmail.com>
2023-12-14 20:11:25 -0300

    deploy pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
         {
             name: "portal343",
             mode: "cluster",
+            instances: 'max',
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
             watch: true,
