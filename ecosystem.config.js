33ee8c7049a3bfc83f1144a410202f40fe5ff0cc
indexadigital <indexadigital.com@gmail.com>
2023-12-21 22:14:55 -0300

    ajuste PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,24 @@
 module.exports = {
     apps : [
         {
-            name: "portal343",
-            mode: "cluster",
-            instances: 4,
+            name: "portal",
             script: 'node_modules/next/dist/bin/next',
-            args: 'startOrRestart',
+            args: 'start',
+            mode: "cluster",
+            instances: 8,
+            max_memory_restart: '1G',            
             watch: true,
+            ignore_watch: ["node_modules"],
             autorestart: true,
             env_development: {
-                "PORT": 3000,
-                "NODE_ENV": "development"
+                PORT: 3000,
+                NEXTAUTH_SECRET: "",
+                NODE_ENV: "development"
             },
             env_production: {
-                "PORT": 3000,
-                "NODE_ENV": "production",
+                PORT: 3000,
+                NEXTAUTH_SECRET: "",
+                NODE_ENV: "production",
             }
         }
     ]
