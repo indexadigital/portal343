38355d792de4de0a6b73911826b1a4695bebf3b9
indexadigital <indexadigital.com@gmail.com>
2025-10-30 17:07:40 -0300

    Tag Adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,600,800&display=swap" />i
-	<script async="true" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4827177938946826" crossOrigin="anonymous"></script>        
+	<script async=true src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4827177938946826" crossOrigin="anonymous"></script>        
       </Head>
       <body>
         <Main />
